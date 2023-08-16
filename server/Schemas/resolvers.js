2b7516447d991cf80def2995f0320bae9031e0b2
VinceR66 <vrook@me.com>
2023-08-15 21:55:31 -0500

    mutations, queries, schemas

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,3 +1,13 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+
+        }
+    }
+};
+
+module.exports = resolvers;
--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,1 +1,24 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        
+    }
+    type User {
+        _id: ID
+              
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
+    token: ID!
+    user: User
+    }
+  
+}
+`;
+
+module.exports = typeDefs;
+
